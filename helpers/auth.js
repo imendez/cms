8b7e70a559d724e3fde2bce89dfb3ce88ff16ba1
Ivan Mendez <ivanmdz@gmail.com>
2015-11-05 13:19:12 -0600

    Auth helper refactor

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,46 +1,45 @@
-module.exports = new function () {
-    var self = this;
-    this.isAuthenticated = function (req) {
+module.exports = (function () {
+    'use strict';
+    var Auth = {};
+
+    Auth.isAuthenticated = function (req) {
         return !!req.session.user;
     };
 
-    this.requiresLogin = function (req, res, next) {
-        if (this.isAuthenticated()) {
-            return next();
-        }
-        return res.redirect('/login?redir=' + req.url);
-    };
-
-    this.accessControl = function (req, res, next) {
-        var user = require('../models/user');
-        var role = require('../models/role');
-        var resource = require('../models/resource');
-        if (!self.isAuthenticated(req)) {
+    Auth.accessControl = function (req, res, next) {
+        var user = require('../models/user'),
+            role = require('../models/role'),
+            resource = require('../models/resource');
+        if (!Auth.isAuthenticated(req)) {
             role.getResources('guest', function (err, resources) {
-                if (self.isPathAuthorized(req.route.path, resources)) {
+                if (err) {
+                    throw err;
+                }
+                if (Auth.isPathAuthorized(req.route.path, resources)) {
                     return next();
                 }
-                else {
-                    return res.redirect('/login?redir=' + req.url);
-                }
+                return res.redirect('/login?redir=' + req.url);
             });
         } else {
             user.findUser(req.session.user.username, function (err, user) {
-                if (err) throw err;
+                if (err) {
+                    throw err;
+                }
+                if (user) {
+                    for (var i = 0; i < user.roles.length; i++) {
+                        var role = user.roles[i];
 
-                for (var i = 0; i < user.roles.length; i++) {
-                    var role = user.roles[i];
-
-                    if (self.isPathAuthorized(req.route.path, role.resources)) {
-                        return next();
+                        if (Auth.isPathAuthorized(req.route.path, role.resources)) {
+                            return next();
+                        }
                     }
                 }
-                self.unauthorized(res);
+                Auth.unauthorized(res);
             });
         }
     };
 
-    this.isPathAuthorized = function (path, resources) {
+    Auth.isPathAuthorized = function (path, resources) {
         for (var i = 0; i < resources.length; i++) {
             if (resources[i].path === path) {
                 return true;
@@ -49,10 +48,13 @@ module.exports = new function () {
         return false;
     };
 
-    this.unauthorized = function(res) {
+    Auth.unauthorized = function (res) {
         res.status(401);
         res.render('error', {
             message: 'Unauthorized'
         });
-    }
-};
+    };
+
+    return Auth;
+}());
+
